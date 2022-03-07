ec26f7e83736f12f2cdba97a7819b2d4bfe0a4a1
KadirErbas <93327468+KadirErbas@users.noreply.github.com>
2022-03-07 16:54:27 +0300

    Add files via upload

---
+++ Sayi_Tahmin_Oyunu/src/Main.java
@@ -0,0 +1,57 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int number = (int) (Math.random() * 100);
+        Scanner input = new Scanner(System.in);
+        int right = 0;
+        int selected;
+        int[] wrong = new int[5];
+        boolean isWin = false;
+        boolean isWrong = false;
+
+        System.out.println(number);
+        while (right < 5) {
+            System.out.print("Lütfen tahmininizi giriniz: ");
+            selected = input.nextInt();
+
+            if (selected < 0 || selected > 99) {
+                System.out.println("Lütfen 0-100 arasında bir değer giriniz.");
+                if (isWrong) {
+                    right++;
+                    System.out.println("Çok fazla hatalı giriş yaptınız. Kalan hakkınız: " + (5 - right));
+                } else {
+                    isWrong = true;
+                    System.out.println("Bir dahaki hatalı girişinizde hakkınızdan düşülecektir.");
+                }
+                continue;
+            }
+
+            if (selected == number) {
+                System.out.println("Tebrikler, doğru tahmin! Tahmin ettiğiniz sayı: " + number);
+                isWin = true;
+                break;
+            } else {
+                System.out.println("Hatalı bir sayı girdiniz!");
+                if (selected > number) {
+                    System.out.println(selected + " sayısı gizli sayıdan büyüktür.");
+                } else {
+                    System.out.println(selected + " sayısı gizli sayıdan küçüktür.");
+                }
+
+                wrong[right++] = selected;
+                System.out.println("Kalan hakkı: " + (5 - right));
+            }
+        }
+
+        if (!isWin) {
+            System.out.println("Kaybettiniz! ");
+            if (!isWrong) {
+                System.out.println("Tahminleriniz: " + Arrays.toString(wrong));
+            }
+        }
+
+    }
+}
