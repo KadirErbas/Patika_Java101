8d684d04db1a43f3853484824044b2aca6586cf9
KadirErbas <93327468+KadirErbas@users.noreply.github.com>
2023-05-30 21:42:07 +0300

    Update Main.java

--- Dizideki_Elemanlari_Siralama/src/Main.java
+++ Dizideki_Elemanlari_Siralama/src/Main.java
@@ -3,14 +3,16 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        // kullanıcı dizinin boyutunu ve elemanlarını belirtir 
         System.out.print("Dizinin boyutunu giriniz: ");
         int n = input.nextInt();
         int[] array = new int[n];
-        System.out.println("Elemanları giriniz.");
+        System.out.println("Elemanları giriniz.");        
         for (int i = 0; i < n; i++){
             System.out.print((i+1)+". eleman: ");
             array[i] = input.nextInt();
         }
+        // olusturulan dizi kucukten buyuge dogru siralanir
         for (int i = 0; i < n; i++){
             for (int j = 0; j < n; j++){
                if (array[i]<array[j]){
@@ -20,6 +22,7 @@ public class Main {
                }
             }
         }
+        // dizinin sıralandıktan sonraki durumunu gosterir
         System.out.print("Sıralama: ");
         for (int a : array) {
             System.out.print(" "+a);
