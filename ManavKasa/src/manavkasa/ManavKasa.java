679a58024092bf146551b34d696c740e0347dccd
Abdulkadir Erbas <93327468+KadirErbas@users.noreply.github.com>
2023-06-19 15:41:10 +0300

    Update ManavKasa.java

--- ManavKasa/src/manavkasa/ManavKasa.java
+++ ManavKasa/src/manavkasa/ManavKasa.java
@@ -5,12 +5,14 @@ import java.util.Scanner;
 public class ManavKasa {
 
     public static void main(String[] args) {
-        
+        // meyvelerin kilo bazında fiyatını tanımlama
         double kiloArmut = 2.14, kiloElma = 3.67, kiloDomates = 1.11, kiloMuz = 0.95, kiloPatlican = 5.00;
+        
         double alınanArmut, alınanElma, alınanDomates, alınanMuz, AlınanPatlican;
         
         Scanner input = new Scanner(System.in);
-        
+
+        // kullanıcıdan aldığı ürünlerin kilosunu alma işlemleri
         System.out.println("Kaç Kilo Armut: ");
         alınanArmut = input.nextDouble();
         
@@ -25,7 +27,8 @@ public class ManavKasa {
 
         System.out.println("Kaç Kilo Patlıcan: ");
         AlınanPatlican = input.nextDouble();
-        
+
+        // tutarın hesaplanıp yazdırılma işlemi
         System.out.println("Tutar: "+((alınanArmut*kiloArmut)+(alınanElma*kiloElma)+(alınanDomates*kiloDomates)+(alınanMuz*kiloMuz)+(AlınanPatlican*kiloPatlican)));
     }
 
